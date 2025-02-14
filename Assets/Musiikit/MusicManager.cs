8cb13972463ac3179fe9f8f88d713db0fe2bf730
juliaviljanen97 <juliaviljanen@hotmail.com>
2025-02-14 11:04:49 +0200

    music

---
+++ Assets/Musiikit/MusicManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+
+    public AudioClip level1Music;
+    public AudioClip level2Music;
+    public AudioClip level3Music;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Musiikki ei katkea scenevaihdoksissa
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.loop = true; // Musiikki luuppaa automaattisesti
+        SceneManager.sceneLoaded += OnSceneLoaded; // Kuunnellaan scenevaihtoa
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        PlayMusicForScene(scene.name);
+    }
+
+    void PlayMusicForScene(string sceneName)
+    {
+        AudioClip newClip = null;
+
+        if (sceneName == "LevelOne")
+            newClip = level1Music;
+        else if (sceneName == "LevelTwo")
+            newClip = level2Music;
+        else if (sceneName == "LevelThree")
+            newClip = level3Music;
+
+        if (newClip != null && audioSource.clip != newClip)
+        {
+            audioSource.clip = newClip;
+            audioSource.Play();
+        }
+    }
+}
